6756d1c3d2c4060b6892200bfd53e63b0dbbfa29
renjiyun <wozaijixu@msn.cn>
2020-10-07 15:27:28 +0800

    初始化

---
+++ src/main/java/com/eggip/hownetworkworks/HownetworkworksApplication.java
@@ -0,0 +1,13 @@
+package com.eggip.hownetworkworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HownetworkworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HownetworkworksApplication.class, args);
+	}
+
+}
